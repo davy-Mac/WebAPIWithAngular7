bbf3d36bfd799b5a803115d9adc55167d714cc00
David Maceda <pankoko@hotmail.com>
2019-02-03 15:18:16 +0800

    inital

---
+++ WebAPIWithAngular7/Controllers/EmployeeController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebAPIWithAngular7.Models;
+
+namespace WebAPIWithAngular7.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        private DBModel db = new DBModel();
+
+        // GET: api/Employee
+        public IQueryable<Employee> GetEmployees()
+        {
+            return db.Employees;
+        }
+
+        // THIS METHOD IS NOT NECESSARY 
+        // GET: api/Employee/5 
+        //[ResponseType(typeof(Employee))]
+        //public IHttpActionResult GetEmployee(int id)
+        //{
+        //    Employee employee = db.Employees.Find(id);
+        //    if (employee == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return Ok(employee);
+        //}
+
+        // PUT: api/Employee/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutEmployee(int id, Employee employee)
+        {
+            //   THIS WILL BE HANDLED BE THE ANGULAR 7 PROJECT
+            //if (!ModelState.IsValid)
+            //{
+            //    return BadRequest(ModelState);
+            //}
+
+            if (id != employee.EmployeeID) // CHECKS IF THE id IN THE EMPLOYEE OBJECT MATCH
+            {
+                return BadRequest(); // IF THEY IDS DON'T MATCH IT WILL RETURN BAD REQUEST
+            }
+
+            db.Entry(employee).State = EntityState.Modified; // IF IT MATCHES THE EntityState IS CHANGED
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent); // THIS EXECUTES IF EVERYTHING GOES WELL
+        }
+
+        // POST: api/Employee
+        [ResponseType(typeof(Employee))]
+        public IHttpActionResult PostEmployee(Employee employee)
+        {
+            // THIS IS HANDLED BY THE ANGULAR PROJECT
+            //if (!ModelState.IsValid)
+            //{
+            //    return BadRequest(ModelState);
+            //}
+
+            db.Employees.Add(employee);
+            db.SaveChanges();
+
+            // IF IT GOES WELL RETURNS THE DETAILS OF THE INSERTED OBJECT
+            return CreatedAtRoute("DefaultApi", new { id = employee.EmployeeID }, employee);
+        }
+
+        // DELETE: api/Employee/5
+        [ResponseType(typeof(Employee))]
+        public IHttpActionResult DeleteEmployee(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+
+            return Ok(employee);
+        }
+
+        // THIS METHOD RELEASES RESOURCES USED BY THE EMPLOYEE CONTROLLER
+        protected override void Dispose(bool disposing) 
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            return db.Employees.Count(e => e.EmployeeID == id) > 0;
+        }
+    }
+}
